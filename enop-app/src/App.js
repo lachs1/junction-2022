3bbe9ee012891b6636b034b35a6b219d22786d93
Alexandra Prinz <alexandraprinz@Alexandra-MBP.home>
2022-11-05 11:22:47 +0200

    UI base

--- enop-app/src/App.js
+++ enop-app/src/App.js
@@ -14,6 +14,7 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -32,7 +33,7 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'Energy prediction',
     },
   },
 };
@@ -43,39 +44,49 @@ export const data = {
   labels,
   datasets: [
     {
-      label: 'Dataset 1',
-      data: [1, 2, 4],
+      label: 'Predicted',
+      data: [3, 2, 4,3,5,10,9],
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
-      label: 'Dataset 2',
-      data: [1, 2, 4],
+      label: 'True prices',
+      data: [1, 1, 3,5,4,6,9],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
   ],
 };
 
+
+
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-      <Line options={options} data={data} />
+      <div className='Sidebar'>
+          <div className='Logo'>
+            ⚡️<div><h1>ENOP</h1></div>
+          </div>
+          <div className='SidebarButton'>
+          🏠<div>Home</div>
+          </div>
+          <div className='SidebarButton'>
+            📉<div>Analytics</div>
+          </div>
+        </div>
+        <div className='Dashboard'>
+        <div className='Headerbox'>
+          <div className='HeaderText'>Hello, Ulla! Today is another perfect day to optimize your energy consumption at the location. </div>
+          </div>
+        <div className='Wrapper'>
+          <Line options={options} data={data} />
+          </div>
+          <div className='Wrapper'>
+          <Line options={options} data={data} />
+          </div>
+        </div>
     </div>
+
   );
 }
 
